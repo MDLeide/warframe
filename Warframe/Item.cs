3bcd6ab2a89880dfa28e7b87cac9ca1232655cf5
Michael Leide <mdleide@gmail.com>
2020-03-16 13:45:13 -0400

    expand item class

--- Warframe/Item.cs
+++ Warframe/Item.cs
@@ -3,5 +3,22 @@
     public class Item
     {
         public string Name { get; set; }
+        public string Type { get; set; }
+        public ItemType TypedType { get; set; }
+        public string Description { get; set; }
+    }
+
+    public enum ItemType
+    {
+        Weapon,
+        WeaponPart,
+        Frame,
+        FramePart,
+        Relic,
+        WeaponBlueprint,
+        FrameBlueprint,
+        WeaponPartBlueprint,
+        FramePartBlueprint,
+        Other
     }
 }
