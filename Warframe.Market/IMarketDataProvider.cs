e2c4388ec61ed98592a10d220f933d119dba1682
Michael Leide <mdleide@gmail.com>
2020-03-16 13:46:53 -0400

    interface for market data provider

---
+++ Warframe.Market/IMarketDataProvider.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Warframe.Market
+{
+    public interface IMarketDataProvider
+    {
+        ItemMarketData GetMarketData(Item item);
+        Dictionary<DateTimeOffset, ItemMarketData> GetMarketData(IEnumerable<Item> items);
+    }
+}
