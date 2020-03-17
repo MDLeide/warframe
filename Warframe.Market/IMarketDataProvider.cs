a723e55011861e5678ccf129d0833e21cff3833c
Michael Leide <mdleide@gmail.com>
2020-03-17 11:24:24 -0400

    expand IMarketData

--- Warframe.Market/IMarketDataProvider.cs
+++ Warframe.Market/IMarketDataProvider.cs
@@ -6,6 +6,11 @@ namespace Warframe.Market
     public interface IMarketDataProvider
     {
         ItemMarketData GetMarketData(Item item);
+        ItemMarketData GetMarketData(Item item, DateTimeOffset inclusiveStart);
+        ItemMarketData GetMarketData(Item item, DateTimeOffset inclusiveStart, DateTimeOffset inclusiveEnd);
+
         Dictionary<DateTimeOffset, ItemMarketData> GetMarketData(IEnumerable<Item> items);
+        Dictionary<DateTimeOffset, ItemMarketData> GetMarketData(IEnumerable<Item> items, DateTimeOffset inclusiveStart);
+        Dictionary<DateTimeOffset, ItemMarketData> GetMarketData(IEnumerable<Item> items, DateTimeOffset inclusiveStart, DateTimeOffset inclusiveEnd);
     }
 }
